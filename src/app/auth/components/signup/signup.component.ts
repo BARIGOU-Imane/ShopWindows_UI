5204ec80c9c2e3c09aa7e27c086882cfa2672b19
BARIGOU-Imane <108489201+BARIGOU-Imane@users.noreply.github.com>
2023-07-17 07:23:40 +0100

    version 2

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -6,31 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  selectedOptionValue !: string;
-  selectedOptionPlaceholder !: string;
+
 
   // Autres méthodes et logique du composant
   ngOnInit() {
   }
-  updateSelectedOption() {
-    switch (this.selectedOptionValue) {
-      case 'rsGoogleBusiness':
-        this.selectedOptionPlaceholder = 'Google Business';
-        break;
-      case 'rsLinkedin':
-        this.selectedOptionPlaceholder = 'LinkedIn';
-        break;
-      case 'rsFacebook':
-        this.selectedOptionPlaceholder = 'Facebook';
-        break;
-      case 'rsInstagram':
-        this.selectedOptionPlaceholder = 'Instagram';
-        break;
-      default:
-        this.selectedOptionPlaceholder = '';
-        break;
-    }
-  }
+
   constructor() { }
 
 
